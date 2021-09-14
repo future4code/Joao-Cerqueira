c79ff0cbcd8df747ad04b6a2af6b7554801cb4b1
jao lucas7 <jaolucas7714@gmail.com>
2021-09-14 19:28:22 -0300

    strings-arrays

---
+++ quinzena1/strings-arrays/exercicios/index.js
@@ -0,0 +1,72 @@
+//Interpretação 
+
+//1-
+//a. undefined          - nenhum valor informado
+//b. null               - valor informado null
+//c. 11                 - array.length para saber comprimento da array
+//d. 0
+//e. não entendi :c
+//f. não entendi :c
+
+//---------------------------
+
+//2- 
+// o valor impresso sera SUBI NUM ONIBUS EM MIRROCOS com seu comprimento (lenght) e maiusculo (toUpperCase) e o A trocado pelo I.
+
+//---------------------------
+
+//Escrita de codigo 
+
+//const nome = prompt("Qual seu nome?")
+//const email = prompt("Qual seu e-mail?")
+
+//console.log("O email", email,"foi cadastrado com sucesso. Seja bem-vinda(o),", nome)
+
+//---------------------------
+//Consegui o exercicio,só o desafio que me pegou e não consegui, deixei abaixo a tentativa :c
+
+//const comidaDoUsuario = prompt("Qual a sua comida preferida?")
+//const comidaPreferida = ["churrasco",
+//                        "sushi",
+//                        "pizza",
+//                        "hamburguer", 
+//                        "macarrão"]
+//const novaComida = comidaPreferida.replaceAll ("sushi", comidaDoUsuario)                     
+
+//console.log("Essas são minhas comidas preferidas:")
+//console.log(comidaPreferida)
+
+//---------------------------
+
+//const listaDeTarefas = []
+//const tarefa1 = prompt("Informe uma tarefa do dia a dia")
+//const tarefa2 = prompt("Informe outra tarefa do dia a dia")
+//const tarefa3 = prompt("Informe mais uma tarefa do dia a dia")
+//const indice = Number(prompt("digite o indice de uma tarefa"))
+
+//listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+//listaDeTarefas.splice(1,1)
+
+//console.log(listaDeTarefas)
+
+//-----------------------------
+
+//DESAFIO
+// não deu cabeça bugou kk
+//const frase = prompt("Digite uma frase:")
+//const array = []
+
+//array.push (frase)
+
+//console.log(array)
+
+//--------------------------------
+
+const frutas =  ["Banana",
+                 "Morango",
+                 "Abacaxi",
+                 "Laranja",
+                 "Ameixa"]
+
+console.log(frutas)                
+console.log(frutas.length)
