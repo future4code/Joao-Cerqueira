ce78e0bb136d4168869b4a34f1f29bd59d29ca21
jao lucas7 <jaolucas7714@gmail.com>
2021-11-19 20:03:11 -0300

    renderização-listas

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -9,28 +9,93 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        key: new Date(),
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/51/51",
+        fotoPost: "https://picsum.photos/200/155",
+      },
+      {
+        key: new Date(),
+        nomeUsuario: "João",
+        fotoUsuario: "https://picsum.photos/51/50",
+        fotoPost: "https://picsum.photos/200/150",
+      },
+      {
+        key: new Date(),
+        nomeUsuario: "Lucas",
+        fotoUsuario: "https://picsum.photos/51/49",
+        fotoPost: "https://picsum.photos/200/149",
+      },
+    ],
+
+    valorKey: new Date(),
+    informacaoUm: "",
+    informacaoDois: "",
+    informacaoTres: "",
+  };
+
+  capturaInformcaoUm = (e) => {
+    this.setState({
+      informacaoUm: e.target.value,
+    });
+  };
+
+  capturaInformacaoDois = (e) => {
+    this.setState({
+      informacaoDois: e.target.value,
+    });
+  };
+
+  capturaInformacaoTres = (e) => {
+    this.setState({
+      informacaoTres: e.target.value,
+    });
+  };
+
+  adicionaInformacao = () => {
+    const copiaPost = [...this.state.posts];
+
+    const novoPost = {
+      nome: this.state.informacaoUm,
+      idade: this.state.informacaoDois,
+      sexo: this.state.informacaoTres
+    };
+
+    copiaPost.push(novoPost);
+    this.setState({ posts: copiaPost })
+  }
+
   render() {
+    const listaDePosts = this.state.posts.map((post) => {
+      console.log(post)
+      return (
+        <>
+          <Post
+            key={post.key}
+            nomeUsuario={post.nomeUsuario}
+            fotoUsuario={post.fotoUsuario}
+            fotoPost={post.fotoPost}
+            nome={post.nome}
+            idade={post.idade}
+            sexo={post.sexo}
+          />
+        </>
+      );
+    });
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'joão'}
-          fotoUsuario={'https://picsum.photos/50/10'}
-          fotoPost={'https://picsum.photos/200/132'}
-        />
-        <Post
-          nomeUsuario={'lucas'}
-          fotoUsuario={'https://picsum.photos/50/15'}
-          fotoPost={'https://picsum.photos/200/130'}
-        />
+        <input onChange={this.capturaInformcaoUm} />
+        <input onChange={this.capturaInformacaoDois} />
+        <input onChange={this.capturaInformacaoTres} />
+        <button onClick={this.adicionaInformacao}>Enviar</button>
+        {listaDePosts}
       </MainContainer>
     );
   }
 }
-
 export default App;
