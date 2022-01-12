947eef1d99bb7650639d53dd14972e130e79360c
jao lucas7 <jaolucas7714@gmail.com>
2022-01-11 22:35:17 -0300

    componentizando astro-match

---
+++ modulo3/astro-match/src/App.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import CardPessoas from './components/CardUsuarios';
+import MatchCard from './components/CardMatch';
+import './App.css';
