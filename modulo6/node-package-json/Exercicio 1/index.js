a44470911485c643222d4dff80b24c56400716ae
jao lucas7 <jaolucas7714@gmail.com>
2022-03-17 22:01:57 -0300

    node-package

---
+++ modulo6/node-package-json/Exercicio 1/index.js
@@ -0,0 +1,11 @@
+const nome = 'João'
+let idade = 21
+
+console.log("Olá,",nome,"! Você tem", idade, "anos.")
+
+// para acessar usamos o node index.js
+// e para acessar process.argv[2] sendo 2 o parâmetro
+
+const idadeFutura = idade + 7
+
+console.log("Olá,",nome,"! Você tem", idade, "anos. Em sete anos você terá", idadeFutura, "anos")
---
+++ modulo6/node-package-json/Exercicio 3/index.js
@@ -0,0 +1,5 @@
+const tarefa = process.argv[2]
+const lista = ['Jogar', 'Comer', 'Estudar', 'Treinar']
+
+const novaLista = lista.push(tarefa)
+console.log(lista); 
