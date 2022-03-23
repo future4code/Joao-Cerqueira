b05b43d4f7e64ae0e8ba6787b6ec543a1cfa5c13
jao lucas7 <jaolucas7714@gmail.com>
2022-03-22 21:08:50 -0300

    intro-ts

---
+++ modulo6/intro-typescript/src/ex2/index.ts
@@ -0,0 +1,6 @@
+function imprimeTresCoresFavoritas():void {
+	const cor1 = String(prompt("Insira sua primeira cor favorita"))
+	const cor2 = String(prompt("Insira sua segunda cor favorita"))
+	const cor3 = String(prompt("Insira sua terceira cor favorita"))
+	console.log([cor1, cor2, cor3])
+     } 
