f3d06afe6bc7c7286f18201a4fb0a9dcb66bf613
jao lucas7 <jaolucas7714@gmail.com>
2022-03-25 22:18:34 -0300

    lista-ts

---
+++ modulo6/lista-ts/src/ex1.ts
@@ -0,0 +1,6 @@
+function imprimeDataNasciento(nome: string, dataNascimento: string): void{
+  let novoArray: string[] = dataNascimento.split("/")
+  console.log("\x1b[31m", `Olá me chamo ${nome}, nasci no dia ${novoArray[0]} do mês de ${novoArray[1]} do ano de ${novoArray[2]}`)
+}
+
+imprimeDataNasciento("João Lucas", "16/08/2000") 
