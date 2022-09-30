fc480c05ae258e474fa1d7653c1dfaf150ae5553
jao lucas7 <jaolucas7714@gmail.com>
2022-09-29 21:13:03 -0300

    case1-entrega

---
+++ cases/case1/src/Colors/collors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+    megasena: 'rgba(0,255,127)',
+    quina: 'rgba(154,205,50)',
+    lotofacil: 'rgba(139,69,19)',
+    lotomania: 'rgba(138,43,226)',
+    timemania: 'rgba(240,128,128)',
+    diadesorte: 'rgba(135,206,250)',
+
+    textColor: 'rgba(225, 225, 255, 1)'
+}
